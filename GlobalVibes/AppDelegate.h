80672e83dc5998adedca2250e136962d5055b943
James Baker <jbaker00@gmail.com>
2018-04-01 11:05:14 -0400

    Initial Commit

---
+++ GlobalVibes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GlobalVibes
+//
+//  Created by James Baker on 4/1/18.
+//  Copyright © 2018 James Baker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GlobalVibes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GlobalVibes
+//
+//  Created by James Baker on 4/1/18.
+//  Copyright © 2018 James Baker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
