dde86324a4f5b3e657518cf2687e55637ad591a5
James Baker <jbaker00@gmail.com>
2018-04-02 18:20:57 -0400

    added more destinations as well as segway to payment or next steps page

--- GlobalVibes/ViewController.m
+++ GlobalVibes/ViewController.m
@@ -22,11 +22,56 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    trips = [NSArray arrayWithObjects:@"Group cruise to Bermuda", @"Group land vacation to Playa Del Carmen", @"Group Disney Cruise to Caribbean", @"Group cruise to Bahama", @"Group land vacation to Thailand", @"Group cruise to Mediterranean", @"Group land vacation to Honolulu",nil];
-    scenes = [NSArray arrayWithObjects:@"1-BrumudaScene.jpeg",@"2-PlayaDelCarmenScene.jpeg",@"3-DisneyCruiseScene.jpeg",@"4-BahamaScene.jpeg",@"5-ThailandScene.jpeg",@"MediterraneanScene.jpeg",@"HawaiiScene.jpeg",nil];
-    flags = [NSArray arrayWithObjects:@"1-BermudaFlag.jpeg",@"2-PlayaDelCarmenFlag.jpeg",@"3-DisneyFlag.jpeg",@"4-BahamasFlag.jpeg",@"5-ThailandFlag.jpeg", @"6-MediterraneanFlag.jpeg", @"7-HawaiiFlag.jpeg", nil];
+    trips = [NSArray arrayWithObjects:@ "Group cruise to Bermuda",
+             @"Group land vacation to Playa Del Carmen",
+             @"Group Disney Cruise to Caribbean",
+             @"Group cruise to Bahama",
+             @"Group land vacation to Thailand",
+             @"Group cruise to Mediterranean",
+             @"Group land vacation to Honolulu",
+             @"Jamican Vacation",
+             @"Fiji Vacation",
+             @"Ft Lauderdale Vacation",
+             @"Los Cabos, Mexico Vacation",
+             @"Santa Barbara California Vacation",
+             @"Mykonos, Greece Vacation",
+             @"Riviera Maya, Mexico Vacation",
+             @"Indonesia Vacation",
+             @"Nassau Paradise Island, Bahamas Vacation",
+             @"St. Vincent & the Grenadines Vacation",
+             @"Ibiza, Spain",
+             @"Orlando, Florida Vacation",
+             @"Cambodia Vacation",
+             nil];
+        //@"Iceland Vacation",@"Paris, France Vacation",@"New Orleans, Louisiana Vacation",@"London, England Vacation",@" Rome, Italy Vacation",nil];
+    scenes = [NSArray arrayWithObjects:@"1-BrumudaScene.jpeg",@"2-PlayaDelCarmenScene.jpeg",@"3-DisneyCruiseScene.jpeg",@"4-BahamaScene.jpeg",@"5-ThailandScene.jpeg",@"6-MediterraneanScene.jpeg",@"7-HawaiiScene.jpeg",nil];
+    flags = [NSArray arrayWithObjects:@"1-BermudaFlag.jpeg",
+             @"2-PlayaDelCarmenFlag.jpeg",
+             @"3-DisneyFlag.jpeg",
+             @"4-BahamasFlag.jpeg",
+             @"5-ThailandFlag.jpeg",
+             @"6-MediterraneanFlag.jpeg",
+             @"7-HawaiiFlag.jpeg",
+             @"8-JamicaFlag.jpeg",
+             @"9-FijiFlag.jpeg",
+             @"10-FlordiaFlag.jpeg",
+             @"11-LosCabosFlag.jpeg",
+             @"12-SantaBarbraFlag.jpeg",
+             @"13-GreeceFlag.jpeg",
+             @"14-RivieraMayaFlag.jpeg",
+             @"15-IndonesiaFlag.jpeg",
+             @"16-NassauFlag.jpeg",
+             @"17-StVincentTheGrenadinesFlag.jpeg",
+             @"18-IbizaFlag.jpeg",
+             @"19-FloridaFlag.jpeg",
+             @"20-CambodiaFlag.jpeg",
+             nil];
 }
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [self performSegueWithIdentifier:@"showBooking" sender:self];
+}
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
